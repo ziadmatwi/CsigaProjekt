2b621c8d94e9912f989fc04d60a7f5813b0167e9
Ponauer Szilvia Mária (Szf_N_2024_2026) <ponauer.szilvia.maria1@diak.szamalk-szalezi.hu>
2025-03-26 10:09:21 +0100

    csiga elso verzio

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -9,5 +9,34 @@ package main;
  * @author PonauerSzilviaMária(
  */
 public class Csiga {
+    private int szin;
+    private String haladas;
+
+    public Csiga(int szin) {
+        this.szin = szin;
+        this.haladas = "";
+    }
+    
+    public void halad(int tavolsag, boolean gyorsitott){
+        String vonal = "";  
+         
+        if (gyorsitott) {
+            vonal = "=";  
+        } else {
+            vonal = "_";  
+        }
+        
+        for (int i = 0; i < tavolsag; i++) {
+            this.haladas +=  " @_//";  
+        }
+    }
+
+    public int getSzin() {
+        return szin;
+    }
+
+    public String getHaladas() {
+        return haladas;
+    }
     
 }
