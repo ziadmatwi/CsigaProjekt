2768295cd0a60a69dc46cafd6026c8f857ef1d9c
Matwi Ziad (Szf_N_2024_2026) <matwi.ziad@diak.szamalk-szalezi.hu>
2025-03-26 09:36:15 +0100

    harmadiksadasd

---
+++ src/main/CsigaVerseny.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author MatwiZiad(Szf_N_2024
+ */
+public class CsigaVerseny {
+    
+}
